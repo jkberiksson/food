6511e71f095b5fc0d62ed9e2ed46af1d8f391815
Jakob Eriksson <jkberiksson@gmail.com>
2024-06-18 14:10:56 +0200

    first commit

---
+++ src/Pages/Thai.js
@@ -0,0 +1,24 @@
+import { motion } from 'framer-motion';
+import Item from '../Components/Item';
+import styled from 'styled-components';
+
+const Thai = ({ items }) => {
+    return (
+        <Container>
+            {items.map((item, index) => (
+                <Item key={index} item={item} />
+            ))}
+        </Container>
+    );
+};
+
+export default Thai;
+
+const Container = styled(motion.div)`
+    padding: 0 2rem;
+    max-width: 1100px;
+    margin: 0 auto;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+    grid-gap: 1rem;
+`;
